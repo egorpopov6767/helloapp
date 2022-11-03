cd90de95747df20a35fffadcbed01f6bedbaab1c
black <black@example.com>
2022-11-03 16:03:29 +0500

    review

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
-print('hello')
 def greetWhite():
     print('hello white')
-print('hello everyone')
 def greetBlack():
     print('hello black')
+print('hello everyone')
+greetWhite()
+greetBlack()
